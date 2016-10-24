c521c611af77ec4f54562a3a2baa7b0fb033145f
froylan valencia <froyvalencia@eduroam-169-233-216-124.ucsc.edu>
2016-10-23 20:37:17 -0700

    first meeting notes 2

--- firstDraftNotes.cpp
+++ firstDraftNotes.cpp
@@ -1,16 +1,33 @@
-Class TYPE<T *> {/
+Class TYPE {/
   //STRIng , CHAR, INT, REAl
-}
+
+};
 class alpha : TYPE {
- flag
-size;
+  flag
+  size;
+};
+
+class char_t : alpha {
+
+}
+
+class str_t : alpha {
 }
 
 class num : type {
-    //flag
-}
+  void add(){}
 
-template< ... >
+};
+
+class dbl_t : num {
+
+};
+
+class int_t : num {
+
+};
+
+template<S *, T *, V *>
 Class VAR {
   
 
@@ -31,18 +48,57 @@ class Machine {
     //parser mechanis -> callout to antother classm
     //< >
     //... > 
-    HahsTable[VAR.id] = OBK(VAR)
-    VAR<><><> t = new VAR<><><>();
+    //HahsTable[VAR.id] = OBK(VAR)
+    //VAR<><><> t = new VAR<><><>();
     
     //submitCommand()
 
 
     //I/O
     //
-}
+  }
+};
 
   //parse()
-  VAR<><><>
+  //VAR<><><>
   
+class parser {
+  parser()
+  //  getName();
+  //getType();
+  //getValue();
+    
 
+};
+
+//undetermined
+//  string tokenizer(){}
+
+class pasrse_Instruction : parser {
+  
+  pasrse_Instruction(String instruction){
+    string instructionType = getInstructionType();
+    List<string> params = getParameters(instructionType);//or get objectType
+    //for param : objType.getParams()) //hopefully by refrence or something
+    int num = getNumOfParams();
+    for(param : params)
+      //get some particular thing
+  }
+  
+};
+
+class parse_Asignment : parser {
+ 
+  parse_Asignmnet(String asignment){
+    inst = toeknzize(null,'$');
+    name = tokenize('$',',' )
+    getName();
+    getType();
+    getValue();
+  }
+};
+
+int main() {
+
+  return 0;
 }
